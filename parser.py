00570db53f184b1412042888e80ddb84c1d273f4
lewandowskijakub <lewyjb@gmail.com>
2020-09-20 11:19:17 +0200

    Providing input and output names

--- parser.py
+++ parser.py
@@ -4,11 +4,18 @@ NAME_INDEX = 0
 
 
 def get_filename(my_args):
-    if len(my_args) >= 2:
+    if len(my_args) >= 3:
         filename = my_args[1]
         return filename
     else:
-        print("[ WARNING ] You should run this program by calling: python parser.py filename")
+        print("[ WARNING ] You should run this program by calling: python parser.py input_file_name output_file_name")
+        return ""
+
+def get_madefile(my_args):
+    if len(my_args) >= 3:
+        madefile = my_args[2]
+        return madefile
+    else:
         return ""
 
 def read_from_file_to_list(filename):
@@ -29,9 +36,10 @@ def get_names(data_list):
 
     return ", ".join(names)
 
-def generate_report(data_list):
+def generate_report(filename, data_list, madefile):
     output = []
-
+    output.append(f"Nazwa Twojego pliku zrodlowego to: {filename}")
+    output.append(f"Nazwa utworzonego pliku to: {madefile}")
     output.append(f"Ilosc danych {len(data_list)}")
     output.append(f"Imiona: {get_names(data_list)}")
 
@@ -45,14 +53,15 @@ def save_report_to_file(report, filename):
 def main():
     my_args = list(sys.argv)
     filename = get_filename(my_args)
+    madefile = get_madefile(my_args)
 
     if len(filename) == 0:
         return
     
     print(f"File to parse: {filename}")
     data_list = read_from_file_to_list(filename)
-    report = generate_report(data_list)
-    save_report_to_file(report, "output.txt")
+    report = generate_report(filename, data_list, madefile)
+    save_report_to_file(report, madefile)
 
 
 if __name__ == "__main__":
