6cad94485af44934e6245fff0cd17454f7824aca
lewandowskijakub <lewyjb@gmail.com>
2020-09-20 10:12:37 +0200

    second change

--- parser.py
+++ parser.py
@@ -3,12 +3,12 @@ import os
 os.system("cls || clear")
 
 def get_filename():
-    if len(sys.argv) >= 2:
+    if len(sys.argv) >= 3:
         input1 = int(sys.argv[1])
         input2 = int(sys.argv[2])
         return input1 + input2
     else:
-        print("You should run this program by calling: python parser.py filename")
+        print("You should run this program by calling: python parser.py input1 input2")
         return ""
 
 
