c3be7fa377414510919de7717e20523a8b6edcdf
lewandowskijakub <lewyjb@gmail.com>
2020-09-20 11:00:50 +0200

    Update done with vim

--- parser.py
+++ parser.py
@@ -1,26 +1,58 @@
 import sys
 
+NAME_INDEX = 0
 
-def get_filename():
-    if len(sys.argv) >= 2:
-        filename = sys.argv[1]
+
+def get_filename(my_args):
+    if len(my_args) >= 2:
+        filename = my_args[1]
         return filename
     else:
         print("[ WARNING ] You should run this program by calling: python parser.py filename")
         return ""
 
-def read from_file_to_list(filename):
-    with open("input.txt","r")
+def read_from_file_to_list(filename):
+    output = []
 
+    with open(filename,"r") as file_to_read:
+        for line in file_to_read.readlines():
+            row = line.replace("\n","").split(" ")
+            output.append(row)
+    
+    return output
+
+def get_names(data_list):
+    names = []
+
+    for line in data_list:
+        names.append(line[NAME_INDEX])
+
+    return ", ".join(names)
+
+def generate_report(data_list):
+    output = []
+
+    output.append(f"Ilosc danych {len(data_list)}")
+    output.append(f"Imiona: {get_names(data_list)}")
+
+    return output
+
+def save_report_to_file(report, filename):
+    with open(filename, "w+") as file_to_read:
+        for report_data in report:
+            file_to_read.write(report_data + "\n")
 
 def main():
-    filename = get_filename()
+    my_args = list(sys.argv)
+    filename = get_filename(my_args)
 
     if len(filename) == 0:
         return
     
     print(f"File to parse: {filename}")
-    
+    data_list = read_from_file_to_list(filename)
+    report = generate_report(data_list)
+    save_report_to_file(report, "output.txt")
 
 
 if __name__ == "__main__":
